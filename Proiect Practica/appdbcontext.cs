870f70ede6efdefaf519b354877bd3a6d32cb50f
Anty <antymarco54@gmail.com>
2026-09-10 13:13:41 +0300

    adaugarea appdbcontext.cs

---
+++ Proiect Practica/appdbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Item> Items { get; set; }
+}
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
--- Proiect Practica/obj/Debug/net8.0/Proiect Practica.AssemblyInfo.cs
+++ Proiect Practica/obj/Debug/net8.0/Proiect Practica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect Practica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad22840b33d49a0a95c4649af3c63bcdbf833ded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect Practica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect Practica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
